92bdf111967cc8ce75d76d985804caabc870c0b3
pierluigi@tukano.it <pierluigi@tukano.it@927897fa-757d-d852-3c9d-3586231020b8>
2011-03-29 21:09:44 +0000

    phong/lambert as default

--- trunk/a3dblenderloader/src/com/ardor3d/extension/model/blender/MaterialStateBuilder.java
+++ trunk/a3dblenderloader/src/com/ardor3d/extension/model/blender/MaterialStateBuilder.java
@@ -1,5 +1,6 @@
 package com.ardor3d.extension.model.blender;
 
+import com.ardor3d.math.ColorRGBA;
 import com.ardor3d.renderer.state.MaterialState;
 import it.tukano.blenderfile.elements.BlenderMaterial;
 
@@ -17,12 +18,48 @@ public class MaterialStateBuilder {
     public MaterialState transformBlenderMaterial(BlenderMaterial blenderMaterial) {
         if(blenderMaterial == null) return null;
 
-        MaterialState state = new MaterialState();
+        final MaterialState state = new MaterialState();
         state.setEnabled(true);
-        state.setAmbient(MathTypeConversions.ColorRGBA(blenderMaterial.getAmbientRgb()));
-        state.setDiffuse(MathTypeConversions.ColorRGBA(blenderMaterial.getRgb()));
-        state.setSpecular(MathTypeConversions.ColorRGBA(blenderMaterial.getSpecularRgb()));
-        state.setShininess(MathTypeConversions.clamp(blenderMaterial.getSpecFactor().floatValue(), 128f, 0f));
+        
+        
+        if(blenderMaterial.hasDiffuseShader(BlenderMaterial.DiffuseShader.LAMBERT)) {
+            setupLambert(state, blenderMaterial);
+        } else {
+            Log.log("Implement this");
+            setupLambert(state, blenderMaterial);
+        }
+
+        if(blenderMaterial.hasSpecularShader(BlenderMaterial.SpecularShader.PHONG)) {
+            setupPhong(state, blenderMaterial);
+        } else {
+            Log.log("Implement this");
+            setupPhong(state, blenderMaterial);
+        }
         return state;
+    }
+
+    /** Applies lambert diffuse shading values to the material state */
+    private void setupLambert(MaterialState state, BlenderMaterial blenderMaterial) {
+        final float refFactor = blenderMaterial.getRefFactor().floatValue(); // 0.0 ... 1.0
+        final ColorRGBA diffuse = MathTypeConversions.ColorRGBA(blenderMaterial.getRgb());
+        final ColorRGBA ambient = MathTypeConversions.ColorRGBA(blenderMaterial.getAmbientRgb());
+
+        diffuse.multiplyLocal(refFactor);
+        state.setDiffuse(diffuse);
+        state.setAmbient(ambient);
+        state.setShininess(0);
+    }
+
+    /** Applies phong specular shading values to the material state */
+    private void setupPhong(MaterialState state, BlenderMaterial blenderMaterial) {
+        final float specFactor = blenderMaterial.getSpecFactor().floatValue(); //0.0 ... 2.0
+        final int hardFactor = blenderMaterial.getHardnessFactor().intValue(); //1 ... 511
+        final ColorRGBA ambient = MathTypeConversions.ColorRGBA(blenderMaterial.getAmbientRgb());
+        final ColorRGBA specular = MathTypeConversions.ColorRGBA(blenderMaterial.getSpecularRgb());
+        final float shininess = hardFactor / 511f * 128f;
+
+        state.setAmbient(ambient);
+        state.setSpecular(specular.multiplyLocal(specFactor / 2f));
+        state.setShininess(shininess);
     }
 }
