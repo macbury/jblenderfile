568bfdba9bdd1f86b28ef91ddea9a1dcec11034a
pierluigi@tukano.it <pierluigi@tukano.it@927897fa-757d-d852-3c9d-3586231020b8>
2011-03-29 18:06:27 +0000

    javadoc

--- a3dblenderloader/src/com/ardor3d/extension/model/blender/MathTypeConversions.java
+++ a3dblenderloader/src/com/ardor3d/extension/model/blender/MathTypeConversions.java
@@ -1,22 +1,35 @@
 package com.ardor3d.extension.model.blender;
 
 import com.ardor3d.math.ColorRGBA;
-import com.ardor3d.math.MathUtils;
 import com.ardor3d.math.Matrix3;
 import com.ardor3d.math.Matrix4;
-import com.ardor3d.math.Quaternion;
 import com.ardor3d.math.Transform;
 import com.ardor3d.math.Vector3;
 import it.tukano.blenderfile.elements.BlenderMatrix3;
 import it.tukano.blenderfile.elements.BlenderMatrix4;
 import it.tukano.blenderfile.elements.BlenderTuple3;
 
+/**
+ * Contains utility methods used to convert math types of the blenderfile project
+ * int ardor3d equivalent
+ * @author pgi
+ */
 public class MathTypeConversions {
 
+    /**
+     * Converts a tuple3 into a vector3
+     * @param tuple the tuple to convert
+     * @return the ardor3d vector equivalent
+     */
     public static Vector3 Vector3(BlenderTuple3 tuple) {
         return new Vector3(tuple.getX().doubleValue(), tuple.getY().doubleValue(), tuple.getZ().doubleValue());
     }
 
+    /**
+     * Converts a blender file matrix into an ardor3d matrix
+     * @param m the matrix to convert
+     * @return the ardor3d matrix equivalent
+     */
     public static Matrix4 Matrix4(BlenderMatrix4 m) {
         return new Matrix4(
                 m.getM00().doubleValue(), m.getM01().doubleValue(), m.getM02().doubleValue(), m.getM03().doubleValue(),
@@ -25,12 +38,11 @@ public class MathTypeConversions {
                 m.getM30().doubleValue(), m.getM31().doubleValue(), m.getM32().doubleValue(), m.getM33().doubleValue());
     }
 
-    public static Transform Transform(BlenderMatrix4 matrix) {
-        Transform t = new Transform().fromHomogeneousMatrix(Matrix4(matrix));
-        t.setTranslation(Vector3(matrix.getTranslation()));
-        return t;
-    }
-
+    /**
+     * Converts a blender matrix3 into an ardor3d matrix
+     * @param matrix the matrix to convert
+     * @return the ardor3d matrix
+     */
     public static Matrix3 Matrix3(BlenderMatrix3 matrix) {
         return new Matrix3(
                 matrix.getM00().doubleValue(), matrix.getM01().doubleValue(), matrix.getM02().doubleValue(),
@@ -38,10 +50,20 @@ public class MathTypeConversions {
                 matrix.getM20().doubleValue(), matrix.getM21().doubleValue(), matrix.getM22().doubleValue());
     }
 
+    /**
+     * Converts a tuple3 representing a xyz euler set into a matrix3
+     * @param eulerRadiansXYZ the angle set to convert
+     * @return the ardor3d matrix
+     */
     public static Matrix3 Matrix3(BlenderTuple3 eulerRadiansXYZ) {
         return new Matrix3().fromAngles(eulerRadiansXYZ.getX().floatValue(), eulerRadiansXYZ.getY().floatValue(), eulerRadiansXYZ.getZ().floatValue());
     }
 
+    /**
+     * Converts a tuple3 representing an rgb color into an ardor3d color
+     * @param rgb the rgb tuple to convert
+     * @return the ardord3 color
+     */
     public static ColorRGBA ColorRGBA(BlenderTuple3 rgb) {
         return new ColorRGBA(rgb.getX().floatValue(), rgb.getY().floatValue(), rgb.getZ().floatValue(), 1);
     }
