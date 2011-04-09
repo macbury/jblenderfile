096aee4bf18f7ae7986d8eae9f89a75442ca8d6b
pierluigi@tukano.it <pierluigi@tukano.it@927897fa-757d-d852-3c9d-3586231020b8>
2011-04-09 18:51:27 +0000

    triangles can push values into buffers

--- trunk/blenderfile/src/it/tukano/blenderfile/elements/BlenderMeshTriangle.java
+++ trunk/blenderfile/src/it/tukano/blenderfile/elements/BlenderMeshTriangle.java
@@ -1,5 +1,8 @@
 package it.tukano.blenderfile.elements;
 
+import java.nio.FloatBuffer;
+import java.util.List;
+
 /**
  * A triangle of a blender mesh
  * @author pgi
@@ -56,4 +59,29 @@ public interface BlenderMeshTriangle {
      * @return the material of this triangle.
      */
     BlenderMaterial getMaterial();
+
+    /**
+     * Fills the given buffer with the position of the vertices of this triangle.
+     * Starts at the current position of the buffer. The buffer remaining space
+     * must be at least 9 (3 values per vertex)
+     * @param coordBuffer the buffer to fill
+     */
+    void pushVerticesCoordinates(FloatBuffer coordBuffer);
+
+    /**
+     * Fills the given buffer with the position of the normals of the vertices
+     * of this triangle. Of.
+     * @param normalBuffer the buffer to fill
+     */
+    void pushNormalsCoordinates(FloatBuffer normalBuffer);
+
+    /**
+     * Fills the given buffer's array with the uv coords of the vertices of this
+     * triangle.
+     * @param texCoordSetNames the list of texture coordinates set layers to use
+     * @param uvBuffer the array of buffers to fill. The length of the array must
+     * be at least texCoordSetNames.size(). The remaining space of the buffers must
+     * be at least 6 (2 values per vertex)
+     */
+    void pushTexCoordSetsCoordinates(List<String> texCoordSetNames, FloatBuffer[] uvBuffer);
 }
